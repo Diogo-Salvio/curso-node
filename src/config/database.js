67a268a10dc0a6c1f948f7c0e1531b72593c9321
Diogo-Salvio <diogosalvio787@gmail.com>
2025-09-29 16:07:30 -0300

    feat: criação da primeira migration

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: '',
+  database: 'teste-dominando-nodejs', 
+  define: {
+    timestamps: true,    
+    underscored: true,
+    underscoredAll: true,   
+  },
+};
