76ed199ae86769ddf7c5e474ab8c43eea82affde
Diogo-Salvio <diogosalvio787@gmail.com>
2025-09-30 13:57:20 -0300

    feat: ultimas edições das migrations e models

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,22 +1,20 @@
 import Sequelize, { Model } from "sequelize"
 
 
-class Contact extends Model {
+class User extends Model {
     static init(sequelize) {
         super.init(
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
-                //status: Sequelize.ENUM("ACTIVE", "ARCHIVED")
+                password_hash: Sequelize.STRING,
+                provider: Sequelize.BOOLEAN,
             },
             {
                 sequelize,
             })
     }
 
-    static associate(models) {
-        this.belongsTo(models.Customer, { foreignKey: "customer_id" })
-    }
 }
 
-export default Contact
+export default User
